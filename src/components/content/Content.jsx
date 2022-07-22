f1fe3ca973e0876a8ea427e598767ba9dd282b31
Harun Doğdu <harundogdu06@gmail.com>
2022-07-22 21:41:21 +0300

    effectArea operations done

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,6 +1,8 @@
 import "./Content.css";
 import { BsInfoCircle } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
 import { featuredItems, imgArray } from "utils/helper";
+import Typed from "react-typed";
 
 const Content = () => {
   return (
@@ -54,6 +56,44 @@ const Content = () => {
             </div>
           </div>
         </div>
+        <div className="effectArea">
+          <div className="effectContainer">
+            <div className="leftCircle"></div>
+            <div className="mainCircle">
+              <div className="mainCircleInner">
+                <div>
+                  <span>Find</span>
+                  <Typed
+                    strings={[
+                      "homes",
+                      "villas",
+                      "apartments",
+                      "aparthotels",
+                      "hotels",
+                    ]}
+                    typeSpeed={40}
+                    backSpeed={40}
+                    loop
+                    showCursor={false}
+                    className="typed"
+                  />
+                </div>
+                <p>for your next trip</p>
+              </div>
+              <button>Discover homes</button>
+            </div>
+            <div className="rightImage">
+              <img
+                src="https://t-cf.bstatic.com/static/img/bh/awareness/campaign/bh_aw_cpg_main_image/b0f5755d0651acbe9d3f3785a874258150b4393b.png"
+                alt="Right"
+              />
+            </div>
+          </div>
+          <button className="effectBtn">
+            <AiOutlineClose />
+          </button>
+        </div>
+        {/* Email Area */}
       </div>
     </div>
   );
