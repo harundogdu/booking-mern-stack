ba77f77db48e9c3541a3b363a49614540df82f13
Harun Doğdu <harundogdu06@gmail.com>
2022-07-22 19:10:36 +0300

    header component done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -21,6 +21,7 @@ export default function Header() {
 
   const [destination, setDestination] = useState("");
 
+  const [openInfoModal, setOpenInfoModal] = useState(false);
   const [info, setInfo] = useState({
     adults: 1,
     children: 0,
@@ -29,6 +30,19 @@ export default function Header() {
 
   const handleOpenDateModal = () => {
     setOpenDateModal(!openDateModal);
+  };
+
+  const handleOpenInfoModal = () => {
+    setOpenInfoModal(!openInfoModal);
+  };
+
+  const handleOption = (name, operation) => {
+    setInfo((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "i" ? info[name] + 1 : info[name] - 1,
+      };
+    });
   };
 
   return (
@@ -83,12 +97,61 @@ export default function Header() {
               />
             )}
           </div>
-          <div className="searchItem">
+          <div className="searchItem" id="dropdown">
             <BsPersonFill />
-            <button className="searchItemBtn">
+            <button className="searchItemBtn" onClick={handleOpenInfoModal}>
               {info.adults} adults · {info.children} children · {info.rooms}{" "}
               room
             </button>
+            {openInfoModal && (
+              <div className="searchItemDropdown">
+                <div className="searchDropdownItem">
+                  <label>Adults</label>
+                  <div className="buttonContainer">
+                    <button
+                      disabled={info.adults <= 1}
+                      onClick={() => handleOption("adults", "d")}
+                    >
+                      -
+                    </button>
+                    <span>{info.adults}</span>
+                    <button onClick={() => handleOption("adults", "i")}>
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div className="searchDropdownItem">
+                  <label>Children</label>
+                  <div className="buttonContainer">
+                    <button
+                      disabled={info.children <= 0}
+                      onClick={() => handleOption("children", "d")}
+                    >
+                      -
+                    </button>
+                    <span>{info.children}</span>
+                    <button onClick={() => handleOption("children", "i")}>
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div className="searchDropdownItem">
+                  <label>Rooms</label>
+                  <div className="buttonContainer">
+                    <button
+                      disabled={info.children <= 1}
+                      onClick={() => handleOption("rooms", "d")}
+                    >
+                      -
+                    </button>
+                    <span>{info.rooms}</span>
+                    <button onClick={() => handleOption("rooms", "i")}>
+                      +
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
           <button className="searchBtn">
             <span>Search</span>
