f8345c9929cff8e86aaaa198679efcf44a4d6d8c
Harun Doğdu <harundogdu06@gmail.com>
2022-07-22 18:05:06 +0300

    index.js and index.css operations done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import { Home } from "pages";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
