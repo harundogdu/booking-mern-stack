0d01f96808298e435bdb7bf2a06c1661885ec09a
Harun Doğdu <harundogdu06@gmail.com>
2022-07-22 16:33:54 +0300

    navbar operations done

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navbarContainer">
+        <div className="navbarLogo">
+          <h1 className="navbarTitle">HDBooking</h1>
+        </div>
+        <div className="navbarButtons">
+          <button className="navbarBtn" onClick={handleLoginClick}>
+            Login
+          </button>
+          <button className="navbarBtn" onClick={handleRegisterClick}>
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
