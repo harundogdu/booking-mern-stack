b50507620437a83dbcecda99d0b356f91bcf189b
Harun Doğdu <harundogdu06@gmail.com>
2022-07-24 18:49:26 +0300

    Hotel Details page designed

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,6 @@ export { default as Content } from "./content/Content";
 export { default as Footer } from "./footer/Footer";
 export { default as HotelList } from "./hotel-list/HotelList";
 export { default as SearchList } from "./search-list/SearchList";
+export { default as DetailsHeader } from "./details-header/DetailsHeader";
+export { default as DetailsGallery } from "./details-gallery/DetailsGallery";
+export { default as DetailsFooter } from "./details-footer/DetailsFooter";
--- src/pages/hotel-details/HotelDetails.jsx
+++ src/pages/hotel-details/HotelDetails.jsx
@@ -1,5 +1,26 @@
+import {
+  Footer,
+  Header,
+  Navbar,
+  DetailsHeader,
+  DetailsGallery,
+  DetailsFooter,
+} from "components";
 import "./HotelDetails.css";
 
 export default function HotelDetails() {
-  return <>Details</>;
+  return (
+    <div className="hotelDetails">
+      <Navbar />
+      <Header type="list" />
+      <section>
+        <main className="hotelDetailsArea">
+          <DetailsHeader />
+          <DetailsGallery />
+          <DetailsFooter />
+        </main>
+      </section>
+      <Footer />
+    </div>
+  );
 }
