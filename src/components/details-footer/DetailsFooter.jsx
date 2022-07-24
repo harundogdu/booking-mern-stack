aae8588a0af637b5a824d187314c179e463ff2be
Harun Doğdu <harundogdu06@gmail.com>
2022-07-24 18:49:09 +0300

    Details Footer component designed

---
+++ src/components/details-footer/DetailsFooter.jsx
@@ -0,0 +1,36 @@
+import "./DetailsFooter.css";
+
+export default function DetailsFooter() {
+  return (
+    <div className="detailsFooter">
+      <div className="footerDetail">
+        <h1>Stay in the heart of Krakow</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+          nobis voluptates iure fuga fugit dolores neque porro modi tempore,
+          possimus dolorum nostrum quas sed sequi! Eum reiciendis nobis porro
+          aliquid reprehenderit, dolores fugit! Fugit deserunt magnam omnis, ea
+          debitis nostrum necessitatibus eveniet rerum optio obcaecati! Quam hic
+          minima provident sint voluptatem quidem? Corrupti exercitationem dolor
+          eum asperiores natus? Nihil nobis nam officiis vel at quis
+          perspiciatis odit, fugiat doloribus adipisci ex aliquid, ratione iure,
+          dolor sit numquam magni eius omnis non. Vel, commodi sunt. Maxime
+          nobis, nostrum sunt ratione, vel fuga, optio voluptatibus veritatis
+          ipsa atque reiciendis error provident illum.
+        </p>
+      </div>
+      <div className="footerButtons">
+        <h1>Perfect for a 9-night stay!</h1>
+        <p className="subTitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+          consequatur vero eum hic repellat aspernatur.
+        </p>
+        <p className="price">
+          <strong>$945</strong>
+          <span>(9 nights)</span>
+        </p>
+        <button>Reserve or Book Now!</button>
+      </div>
+    </div>
+  );
+}
