79178fb77e967975ed995af1a26f70faf3a5b652
Harun Doğdu <harundogdu06@gmail.com>
2022-07-26 00:44:09 +0300

    user controller methods created

---
+++ server/controllers/users.js
@@ -0,0 +1,57 @@
+import User from "../models/User.js";
+import { createError } from "../utils/error.js";
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return next(createError(404, "User not found"));
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      success: true,
+      message: "User retrieved successfully",
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
