eaaa5fe7bda395f97c1ad450d269691691cea1d7
Harun Doğdu <harundogdu06@gmail.com>
2022-07-25 18:43:10 +0300

    hotel models created

---
+++ server/models/Hotel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const HotelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: Number,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  rooms: {
+    type: [String],
+  },
+  cheapestPrice: {
+    type: Number,
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default Hotel = mongoose.model("Hotel", HotelSchema);
