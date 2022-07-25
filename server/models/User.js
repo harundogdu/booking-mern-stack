226691ef64dd8f40d6a5060d41ee594a40014c05
Harun Doğdu <harundogdu06@gmail.com>
2022-07-25 18:43:20 +0300

    user models created

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default User = mongoose.model("User", UserSchema);
