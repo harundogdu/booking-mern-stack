95f45bc6be4ce2462b7ff1437bb3c579eaa5169c
Harun Doğdu <harundogdu06@gmail.com>
2022-07-25 20:47:49 +0300

    login and register operation done

---
+++ server/controllers/auth.js
@@ -0,0 +1,60 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found"));
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect)
+      return next(
+        createError(404, "Wrong password or username, please try again")
+      );
+
+    const token = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET_KEY
+    );
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({
+        success: true,
+        message: "User logged in successfully",
+        token,
+        user: { ...otherDetails },
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const register = async (req, res, next) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
+    await newUser.save();
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: newUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
