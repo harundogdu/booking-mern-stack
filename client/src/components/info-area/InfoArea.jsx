ae77121a4587cb3abff10e2272e8f286165c70fe
Harun Doğdu <harundogdu06@gmail.com>
2022-07-27 20:45:16 +0300

    info area component created

---
+++ client/src/components/info-area/InfoArea.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BsInfoCircle } from "react-icons/bs";
+
+export default function InfoArea() {
+  return (
+    <div className="infoArea">
+      <BsInfoCircle />
+      <span>
+        Get the advice you need. Check the latest COVID-19 restrictions before
+        you travel. Learn more
+      </span>
+    </div>
+  );
+}
