1daf4ced2dd67077059d4f71c5ec10ba0c32d987
Harun Doğdu <Harundogdu06@gmail.com>
2022-09-30 19:07:56 +0300

    fix(client) : some error fixed

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,8 +1,9 @@
-import axios from "axios";
-import { useSearch, useFetch } from "context/SearchContext";
-import { useEffect, useState } from "react";
-import { AiOutlineClose } from "react-icons/ai";
-import "./Reserve.css";
+import axios from 'axios';
+import { useSearch } from 'context/SearchContext';
+import useFetch from 'hooks/useFetch';
+import { useEffect, useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import './Reserve.css';
 
 export default function Reserve({ openModal, setOpenModal, hotelId }) {
   const { data } = useFetch(`/hotels/hotelsRooms/${hotelId}`);
@@ -10,18 +11,18 @@ export default function Reserve({ openModal, setOpenModal, hotelId }) {
   const [selectedRooms, setSelectedRooms] = useState([]);
 
   useEffect(() => {
-    const body = document.querySelector("body");
+    const body = document.querySelector('body');
     window.scrollTo(0, 0);
-    body.style.overflow = openModal ? "hidden" : "auto";
+    body.style.overflow = openModal ? 'hidden' : 'auto';
   }, [openModal]);
 
-  const handleSelect = (e) => {
+  const handleSelect = e => {
     const checked = e.target.checked;
     const roomId = e.target.value;
     setSelectedRooms(
       checked
         ? [...selectedRooms, roomId]
-        : selectedRooms.filter((room) => room !== roomId)
+        : selectedRooms.filter(room => room !== roomId)
     );
   };
 
@@ -42,8 +43,8 @@ export default function Reserve({ openModal, setOpenModal, hotelId }) {
 
   const allDates = getDatesInRange(dates[0].startDate, dates[0].endDate);
 
-  const isAvailable = (roomNumber) => {
-    const isFound = roomNumber.unavailableDates.some((date) =>
+  const isAvailable = roomNumber => {
+    const isFound = roomNumber.unavailableDates.some(date =>
       allDates.includes(new Date(date).getTime())
     );
     return !isFound;
@@ -52,56 +53,54 @@ export default function Reserve({ openModal, setOpenModal, hotelId }) {
   const handleReserveClick = async () => {
     try {
       await Promise.all(
-        selectedRooms.map(async (roomNumber) => {
+        selectedRooms.map(async roomNumber => {
           const response = axios.put(`/rooms/availability/${roomNumber}`, {
-            availability: allDates.map((date) => date.toISOString()),
+            availability: allDates.map(date => date.toISOString())
           });
           return response.data;
         })
       );
     } catch (error) {
-      alert("Something went wrong");
+      alert('Something went wrong');
     }
   };
 
   return (
-    <div className="reserve">
-      <div className="rContainer">
-        <div className="rClose">
+    <div className='reserve'>
+      <div className='rContainer'>
+        <div className='rClose'>
           <button onClick={() => setOpenModal(false)}>
             <AiOutlineClose />
           </button>
         </div>
-        <div className="rInner">
-          <div className="rSlogan">Select your rooms:</div>
-          <div className="form">
-            {data.map((item) => (
-              <div className="rRooms">
-                <div className="rTitle">{item.title}</div>
-                <div className="rInpt">
-                  <div className="rTextContainer">
-                    <div className="rText">{item.description}</div>
-                    <div className="rMax">
+        <div className='rInner'>
+          <div className='rSlogan'>Select your rooms:</div>
+          <div className='form'>
+            {data.map(item => (
+              <div key={item.title} className='rRooms'>
+                <div className='rTitle'>{item.title}</div>
+                <div className='rInpt'>
+                  <div className='rTextContainer'>
+                    <div className='rText'>{item.description}</div>
+                    <div className='rMax'>
                       Max people : <strong>{item.maxPeople}</strong>
                     </div>
                   </div>
-                  <div className="rInputContainer">
-                    {item.roomNumbers.map((roomNumber) => (
-                      <>
-                        <div className="rInput">
-                          <p>{roomNumber.number}</p>
-                          <input
-                            type="checkbox"
-                            value={roomNumber._id}
-                            onChange={handleSelect}
-                            disabled={!isAvailable(roomNumber)}
-                          />
-                        </div>
-                      </>
+                  <div className='rInputContainer'>
+                    {item.roomNumbers.map(roomNumber => (
+                      <div key={roomNumber.number} className='rInput'>
+                        <p>{roomNumber.number}</p>
+                        <input
+                          type='checkbox'
+                          value={roomNumber._id}
+                          onChange={handleSelect}
+                          disabled={!isAvailable(roomNumber)}
+                        />
+                      </div>
                     ))}
                   </div>
                 </div>
-                <div className="rPrice">${item.price}</div>
+                <div className='rPrice'>${item.price}</div>
               </div>
             ))}
           </div>
